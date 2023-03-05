7a3e45311eaa5cfe58ea7f2fc8e078d7c8e7d64e
rishabh jain <goyal.rishabh12@gmail.com>
2023-03-05 14:28:45 +0530

    aa

---
+++ src/ayush.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ayush() {
+    return (
+        <div>
+            <h1>wwwwwwwwwwwwwwwwwwwwwwwwwwww Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae eius cumque sunt dicta doloremque recusandae dolorum consequuntur ab. Reiciendis iusto tempora nobis, asperiores tempore totam aut dolor magni ullam similique!</h1>
+        </div>
+    )
+}
